68e4d02450369eb6fdc8ae3c3f26a9de66ab08dd
SurytheGreat <24je0705@iitism.ac.in>
2026-04-05 23:53:29 +0530

    classes implemented

--- DeepMatrix/models.py
+++ DeepMatrix/models.py
@@ -12,16 +12,57 @@ The DeepMatrix environment is a simple test environment that echoes back message
 
 from openenv.core.env_server.types import Action, Observation
 from pydantic import Field
+import numpy as np
 
-
+n =10;
 class DeepmatrixAction(Action):
     """Action for the Deepmatrix environment - just a message to echo."""
-
-    message: str = Field(..., description="Message to echo back")
+    items_to_buy : list[int] = Field(
+        default=np.zeros(n, dtype=int).tolist(),
+        min_length=n,
+        max_length=n,
+        description="number of items to buy"
+    )
+    
 
 
 class DeepmatrixObservation(Observation):
     """Observation from the Deepmatrix environment - the echoed message."""
 
-    echoed_message: str = Field(default="", description="The echoed message")
-    message_length: int = Field(default=0, description="Length of the echoed message")
+    Inventory: list[int] = Field(
+        default=np.zeros(n, dtype=int).tolist(),
+        min_length=n,
+        max_length=n,
+        description="number of items in inventory"
+    )
+    expiry_time : list[int] = Field(
+        default=np.zeros(n, dtype=int).tolist(),
+        min_length=n,
+        max_length=n,
+        description="expiry time for the items in inventory"
+    )
+    demand : list[int] = Field(
+        default=np.zeros(n, dtype=int).tolist(),
+        min_length=n,
+        max_length=n,
+        description="demand for the items in inventory"
+    )
+    buying_price : list[int] = Field(
+        default=np.zeros(n, dtype=int).tolist(),
+        min_length=n,
+        max_length=n,
+        description="buying price for the items in inventory"
+    )
+    arrival_time : list[int] = Field(
+        default=np.zeros(n, dtype=int).tolist(),
+        min_length=n,
+        max_length=n,
+        description="arrival time for the items in inventory"
+    )
+    in_transit : list[int] = Field(
+        default=np.zeros(n, dtype=int).tolist(),
+        min_length=n,
+        max_length=n,
+        description="number of items in transit"
+    )
+    budget : int = Field(default=0, description="budget for purchasing items")
